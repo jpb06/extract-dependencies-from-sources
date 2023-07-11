14c2070e9ec60e39c7f61a486b3a80779c89efc2
JP Bois <jp.bois.06@outlook.fr>
2023-07-11 20:03:21 +0200

    chore: getExternalImports refac (#11)

--- src/logic/codebase-dependencies/path/file/get-file-dependencies.ts
+++ src/logic/codebase-dependencies/path/file/get-file-dependencies.ts
@@ -1,31 +1,29 @@
 import { pipe } from '@effect/data/Function';
+import * as Option from '@effect/data/Option';
+import * as Predicate from '@effect/data/Predicate';
+import * as Array from '@effect/data/ReadonlyArray';
+import * as Str from '@effect/data/String';
 import * as Effect from '@effect/io/Effect';
 import { readFile } from 'fs-extra';
 
 const depsRegex = new RegExp(/^import.*(from)? ['"](.*)['"].*$/, 'gm');
 
+const isRelative = Predicate.or(Str.startsWith('./'), Str.startsWith('../'));
+
 const getExternalImports = (fileContent: string) =>
   pipe(
-    Effect.sync(() => depsRegex.exec(fileContent)),
-    Effect.flatMap((firstMatch) =>
-      Effect.loop(firstMatch, {
-        step: () => depsRegex.exec(fileContent),
-        while: (newMatch) => newMatch !== null,
-        body: (match) => {
-          if (!match) {
-            return Effect.succeed(null);
-          }
-
-          const dep = match[2];
-          if (!dep.startsWith('./') && !dep.startsWith('../')) {
-            return Effect.succeed(dep);
-          }
-
-          return Effect.succeed(null);
-        },
-      }),
-    ),
-    Effect.map((imports) => imports.filter((i) => i !== null) as string[]),
+    Effect.loop(Option.fromNullable(depsRegex.exec(fileContent)), {
+      while: Option.isSome,
+      body: (match) =>
+        Effect.sync(() =>
+          match.pipe(
+            Option.flatMap(Array.get(2)),
+            Option.filter(Predicate.not(isRelative)),
+          ),
+        ),
+      step: () => Option.fromNullable(depsRegex.exec(fileContent)),
+    }),
+    Effect.map(Array.compact),
   );
 
 export const getFileDependencies = (path: string) =>
