083eca3326131325f7d418bba1a279c8c38faf5f
jpb06 <jp.bois.06@outlook.fr>
2025-09-06 14:08:29 +0200

    chore: pinning actions deps (#110)

--- src/cli/args/options-validation/index.ts
+++ src/cli/args/options-validation/index.ts
@@ -1,6 +1,4 @@
 export * from './external-dependencies/index.js';
 export type { ExternalDeps } from './external-dependencies/logic/read-external-dependencies.js';
-
 export * from './paths/index.js';
-
 export * from './root-package-json/index.js';
